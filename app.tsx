433e480daa5a27c7ddd149cd43142a30a8ca2efd
Seth Trowbridge <seth111@gmail.com>
2022-12-02 16:53:59 -0500

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -7,3 +7,7 @@ export default (inRoot:Element)=>
 {
     render(<div class="p-4 bg-blue-500 text-white m-big">sup.</div>, inRoot);
 };
+
+
+import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+serve((req) => new Response(React.createElement));
